869ac06d339efba6a3a89ba5f83811b1a6e963c5
kelechi prosper <prosperkelechi50@gmail.com>
2022-06-24 08:12:33 +0100

    i

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // log to console
-console.log('Hello World');
-console.log(123);
-console.log(true);
-var greeting = 'hello';
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = 'hello';
+// console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+// console.clear();
+// console.warn('this is a warning');
+
+// var, let, const
+
+var name = 'High Chief';
+console.log(name);
+name = 'Steve John'
+console.log(name);
+
+// Init Var
+var greeting;
 console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
+greeting = 'Hello';
+console.log(greeting);
